df465fad919399c532a19867445d264fb3d4644c
AriDEV <michalik.andrej@centrum.sk>
2011-06-22 23:11:03 +0200

    Core/DataStores: Updated Aiction House And BarberShop Sructure.
    Signed-off-by: AriDEV <michalik.andrej@centrum.sk>

--- src/server/game/DataStores/DBCStructure.h
+++ src/server/game/DataStores/DBCStructure.h
@@ -591,8 +591,7 @@ struct AuctionHouseEntry
     uint32    faction;                                      // 1 id of faction.dbc for player factions associated with city
     uint32    depositPercent;                               // 2 1/3 from real
     uint32    cutPercent;                                   // 3
-    //char*     name[16];                                   // 4-19
-                                                            // 20 string flag, unused
+    //DBCString     name;                                   // 4
 };
 
 struct BankBagSlotPricesEntry
@@ -605,14 +604,12 @@ struct BarberShopStyleEntry
 {
     uint32  Id;                                             // 0
     uint32  type;                                           // 1 value 0 -> hair, value 2 -> facialhair
-    //char*   name[16];                                     // 2-17 name of hair style
-    //uint32  name_flags;                                   // 18
-    //uint32  unk_name[16];                                 // 19-34, all empty
-    //uint32  unk_flags;                                    // 35
-    //float   CostMultiplier;                               // 36 values 1 and 0.75
-    uint32  race;                                           // 37 race
-    uint32  gender;                                         // 38 0 -> male, 1 -> female
-    uint32  hair_id;                                        // 39 real ID to hair/facial hair
+    //DBCString   name;                                     // 2 name of hair style
+    //uint32  unk_name;                                     // 3, all empty
+    //float   CostMultiplier;                               // 4 values 1 and 0.75
+    uint32  race;                                           // 5 race
+    uint32  gender;                                         // 6 0 -> male, 1 -> female
+    uint32  hair_id;                                        // 7 real ID to hair/facial hair
 };
 
 struct BattlemasterListEntry
