0c7c06a0560916e8822248bf1750da2b578f918c
AriDEV <michalik.andrej@centrum.sk>
2011-07-11 18:35:54 +0200

    Core/DB: Deleted Outdated Updates. Core/DB: Fixed Arena SQL Update To T-SQL.
    Signed-off-by: AriDEV <michalik.andrej@centrum.sk>

--- sql/updates/world/2011_07_11_0_arena_system.sql
+++ sql/updates/world/2011_07_11_0_arena_system.sql
@@ -1,5 +1,8 @@
-DELETE FROM `command` WHERE `name`='flusharenapoints'
-INSERT INTO `command` (`name`, `security`, `help`) VALUES ('weekreset', 3, 'Syntax: .weekreset\r\nForce Week to finish, and reset WeeGames / WeekWins.');
+DELETE FROM command WHERE name='flusharenapoints';
+DELETE FROM command WHERE name='weekreset';
+INSERT INTO command (name, security, help) VALUES 
+('weekreset', 3, 'Syntax: .weekreset\r\nForce Week to finish, and reset WeeGames / WeekWins.');
 
-INSERT INTO `trillium_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES (1136, 'Week is finished. Arena Stats (Week Games/Wins) set to 0.', '', '', '', '', '', '', '', '');
-
+DELETE FROM trillium_string WHERE entry=1136;
+INSERT INTO trillium_string (entry, content_default) VALUES 
+(1136, 'Week is finished. Arena Stats (Week Games/Wins) set to 0.');
