4b48af3e77fbbd270db091e3ed8fb5d38f27361b
AriDEV <michalik.andrej@centrum.sk>
2011-07-12 14:19:38 +0200

    Core/DB: Deleted Updates. Allready Added To TrilliumDB-406.13623_r7. Core/DB: Added Readme Hot Wo Use This Folder.
    Signed-off-by: AriDEV <michalik.andrej@centrum.sk>

--- sql/updates/world/2011_07_11_0_arena_system.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM command WHERE name='flusharenapoints';
-DELETE FROM command WHERE name='weekreset';
-INSERT INTO command (name, security, help) VALUES 
-('weekreset', 3, 'Syntax: .weekreset\r\nForce Week to finish, and reset WeeGames / WeekWins.');
-
-DELETE FROM trillium_string WHERE entry=1136;
-INSERT INTO trillium_string (entry, content_default) VALUES 
-(1136, 'Week is finished. Arena Stats (Week Games/Wins) set to 0.');
