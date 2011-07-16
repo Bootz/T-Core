451be153b96486b800fa686f4ddc9a5c02f26915
AriDEV <michalik.andrej@centrum.sk>
2011-07-16 19:45:21 +0200

    Core/Opcode: Added CMSG_QUESTGIVER_STATUS_MULTIPLE_QUERY
    Signed-off-by: AriDEV <michalik.andrej@centrum.sk>

--- src/server/game/Server/Protocol/Opcodes.h
+++ src/server/game/Server/Protocol/Opcodes.h
@@ -1054,7 +1054,7 @@ enum Opcodes
     CMSG_TOTEM_DESTROYED                             = 0x03EB7, // 14333
     CMSG_EXPIRE_RAID_INSTANCE                        = 0x101A7, //
     CMSG_NO_SPELL_VARIANCE                           = 0x101A8, //
-    CMSG_QUESTGIVER_STATUS_MULTIPLE_QUERY            = 0x02DAC, //
+    CMSG_QUESTGIVER_STATUS_MULTIPLE_QUERY            = 0x0A2FA, // 14333
     SMSG_QUESTGIVER_STATUS_MULTIPLE                  = 0x0F5C0, //
     CMSG_SET_PLAYER_DECLINED_NAMES                   = 0x03EE6, // 14333
     SMSG_SET_PLAYER_DECLINED_NAMES_RESULT            = 0x0BAAC, //
