a0c906075d230f37cea5d6c03e8720ce791c8786
pek2011 <pek2011@foxmail.com>
2011-07-26 13:52:18 +0800

    Core/Misc: Drop table achievement_dbc
    Signed-off-by: pek2011 <pek2011@foxmail.com>

--- src/server/game/DataStores/DataFormat.h
+++ src/server/game/DataStores/DataFormat.h
@@ -19,8 +19,6 @@
 #define TRILLIUM_DATAFORMAT_H
 
 const char Achievementfmt[]="niixsxiixixxii";
-//const std::string CustomAchievementfmt="ppppppppppppppp";
-//const std::string CustomAchievementIndex = "ID";
 const char AchievementCriteriafmt[]="niiiiiiiixsiiiiixxxxxxx";
 const char AreaTableEntryfmt[]="iiinixxxxxisixxxxxxxxxxxxx";
 const char AreaGroupEntryfmt[]="niiiiiii";
