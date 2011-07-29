1c77a05ad5b648555f39d354bbfecff299affb9b
pek2011 <pek2011@foxmail.com>
2011-07-29 16:34:12 +0800

    Core/Quest: Fix QuestGiverStatus, status icon is displayed correctly.
    Signed-off-by: pek2011 <pek2011@foxmail.com>

--- src/server/game/Quests/QuestDef.h
+++ src/server/game/Quests/QuestDef.h
@@ -109,17 +109,18 @@ enum QuestStatus
 
 enum __QuestGiverStatus
 {
-    DIALOG_STATUS_NONE                     = 0,
-    DIALOG_STATUS_UNAVAILABLE              = 1,
-    DIALOG_STATUS_LOW_LEVEL_AVAILABLE      = 2,
-    DIALOG_STATUS_LOW_LEVEL_REWARD_REP     = 3,
-    DIALOG_STATUS_LOW_LEVEL_AVAILABLE_REP  = 4,
-    DIALOG_STATUS_INCOMPLETE               = 5,
-    DIALOG_STATUS_REWARD_REP               = 6,
-    DIALOG_STATUS_AVAILABLE_REP            = 7,
-    DIALOG_STATUS_AVAILABLE                = 8,
-    DIALOG_STATUS_REWARD2                  = 9,             // no yellow dot on minimap
-    DIALOG_STATUS_REWARD                   = 10             // yellow dot on minimap
+    DIALOG_STATUS_NONE                     = 0x0000,
+    DIALOG_STATUS_UNK                      = 0x0001,
+    DIALOG_STATUS_UNAVAILABLE              = 0x0002,
+    DIALOG_STATUS_LOW_LEVEL_AVAILABLE      = 0x0004,
+    DIALOG_STATUS_LOW_LEVEL_REWARD_REP     = 0x0008,
+    DIALOG_STATUS_LOW_LEVEL_AVAILABLE_REP  = 0x0010,
+    DIALOG_STATUS_INCOMPLETE               = 0x0020,
+    DIALOG_STATUS_REWARD_REP               = 0x0040,
+    DIALOG_STATUS_AVAILABLE_REP            = 0x0080,
+    DIALOG_STATUS_AVAILABLE                = 0x0100,
+    DIALOG_STATUS_REWARD2                  = 0x0200,         // no yellow dot on minimap
+    DIALOG_STATUS_REWARD                   = 0x0400          // yellow dot on minimap
 };
 
 enum __QuestFlags
