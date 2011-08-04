ea132c31b108ed99c4afc88e528b2654fc7601a7
Pointerxx <marvin.wichmann@web.de>
2011-08-04 23:47:27 +0200

    Core/Command: Add useful command for Database Developer to create Emotes with commadn and SQL Output -> Needs testing & Feedback -> Add more GM Levels for allowing gmlevel > 3 in command table -> Add some comments
    Signed-off-by: Pointerxx <marvin.wichmann@web.de>

--- src/server/shared/Common.h
+++ src/server/shared/Common.h
@@ -163,7 +163,9 @@ enum AccountTypes
     SEC_MODERATOR      = 1,
     SEC_GAMEMASTER     = 2,
     SEC_ADMINISTRATOR  = 3,
-    SEC_CONSOLE        = 4                                  // must be always last in list, accounts must have less security level always also
+	SEC_LEVEL_4        = 4,
+	SEC_LEVEL_5        = 5,
+    SEC_CONSOLE        = 6      	// must be always last in list, accounts must have less security level always also
 };
 
 enum LocaleConstant
@@ -201,7 +203,7 @@ typedef std::vector<std::string> StringVector;
 #endif
 
 #ifndef M_PI
-#define M_PI            3.14159265358979323846f
+#define M_PI            3.14159265358979323846f		// Math Pi 
 #endif
 
 #define MAX_QUERY_LEN 32*1024
