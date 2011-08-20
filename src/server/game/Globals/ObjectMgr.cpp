0ee5464eccbc521e2381770bf2a61d7ece3bcaf5
Venugh <Venugh@gmx.net>
2011-08-20 14:30:33 +0200

    Partly Revert "Core/Creature Fix Max Vednor Items, should fix a client crash."
    This reverts commit e76174c4fd49a147f42069e9bc47b00dba96cdb6.

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -8494,6 +8494,15 @@ bool ObjectMgr::IsVendorItemValid(uint32 vendor_entry, uint32 item_id, int32 max
         return false;
     }
 
+    if (vItems->GetItemCount() >= MAX_VENDOR_ITEMS)
+    {
+        if (pl)
+            ChatHandler(pl).SendSysMessage(LANG_COMMAND_ADDVENDORITEMITEMS);
+        else
+            sLog->outErrorDb("Table `npc_vendor` has too many items (%u >= %i) for vendor (Entry: %u), ignore", vItems->GetItemCount(), MAX_VENDOR_ITEMS, vendor_entry);
+        return false;
+    }
+
     return true;
 }
 
