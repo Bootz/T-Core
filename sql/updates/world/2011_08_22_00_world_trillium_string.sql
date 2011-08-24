2009bbd39275c8bb51ee6129cdc8950b7dc91fd4
Venugh <Venugh@gmx.net>
2011-08-24 17:47:03 +0200

    Merge Trinity to: Fix diminishing for spells with group DIMINISHING_LIMITONLY.

---
+++ sql/updates/world/2011_08_22_00_world_trillium_string.sql
@@ -0,0 +1,4 @@
+DELETE FROM `trillium_string` WHERE `entry`=1136;
+INSERT INTO `trillium_string` (`entry`,`content_default`,`content_loc1`,`content_loc2`,`content_loc3`,`content_loc4`,`content_loc5`,`content_loc6`,`content_loc7`,`content_loc8`)
+VALUES
+(1137,'Character %s has never been banned!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
