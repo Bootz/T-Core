6b3a35a50ea2a41ce34fab651aa2ca6958c5710a
Venugh <Venugh@gmx.net>
2011-08-24 16:26:04 +0200

    DB: Drop duplicate folders.

--- sql/old/characters/2011_08_19_01_character_talentspec.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE characters
-  add column `talentSpec` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT 0 AFTER `achievementPoints`;
--- sql/old/world/2011_08_15_00_world_creature_template.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `creature_template`  ADD COLUMN `trainer_id` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' AFTER `family`;
