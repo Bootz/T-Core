38ce98f01b7b43e9ee1aff1fafdc56d917c559a4
yezi <yezi@foxmail.com>
2011-08-27 10:09:12 +0800

    Core/Trainer: Fix Trainer, now you can use Trainer learn spell.
    Signed-off-by: yezi <yezi@foxmail.com>

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -14150,9 +14150,7 @@ void Player::OnGossipSelect(WorldObject* source, uint32 gossipListId, uint32 men
         return;
 
     int32 cost = int32(item->BoxMoney);
-    int32 dualSpecCost = 100000;
-    bool dualSpec = false;
-    if (!HasEnoughMoney(cost) || !HasEnoughMoney(dualSpecCost))
+    if (!HasEnoughMoney(cost))
     {
         SendBuyError(BUY_ERR_NOT_ENOUGHT_MONEY, 0, 0, 0);
         PlayerTalkClass->SendCloseGossip();
@@ -14198,7 +14196,6 @@ void Player::OnGossipSelect(WorldObject* source, uint32 gossipListId, uint32 men
         case GOSSIP_OPTION_LEARNDUALSPEC:
             if (GetSpecsCount() == 1 && getLevel() >= sWorld->getIntConfig(CONFIG_MIN_DUALSPEC_LEVEL))
             {
-                dualSpec = true; // Use for Money modify
                 // Cast spells that teach dual spec
                 // Both are also ImplicitTarget self and must be cast by player
                 CastSpell(this, 63680, true, NULL, NULL, GetGUID());
@@ -14256,12 +14253,7 @@ void Player::OnGossipSelect(WorldObject* source, uint32 gossipListId, uint32 men
         }
     }
 
-    if (dualSpec)
-    {
-        ModifyMoney(-dualSpecCost);
-    }
-    else
-        ModifyMoney(-cost);
+    ModifyMoney(-cost);
 }
 
 uint32 Player::GetGossipTextId(WorldObject* source)
