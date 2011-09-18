e5e171dd614e8152da27989652029327f0b24775
Venugh <Venugh@gmx.net>
2011-09-18 21:48:39 +0200

    Db/SQL: Drop old updates. This updates are not good.

--- sql/updates/world/2011_09_05_03_world_spell_proc_event.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `spell_proc_event` WHERE entry IN ('86303', '86304');
-insert into `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) values('86303','0','0','0','0','0','1048576','0','0','50','0');
-insert into `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) values('86304','0','0','0','0','0','1048576','0','0','100','0');
